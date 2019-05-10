10c9c24c258306b1da0abc8d5187597029817bdb
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-10 11:57:29 +0300

    Add base.html file to the templates

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,10 @@
 from django.http  import HttpResponse
+from django.shortcuts import render
 import datetime as dt
 
 # Create your views here.
 def mygallery(request):
     date = dt.date.today()
-    photos = Image.get_all()
+    # photos = Image.get_all()
 
-    return render(request,'all-photos/home.html',{"date": date,"photos":photos})
+    return render(request,'welcome.html',{"date": date})
