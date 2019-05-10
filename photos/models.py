a56425dad7033ebb5a4ae3cab3cd8be1f14971cd
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-10 16:48:59 +0300

    Creating category model

--- photos/models.py
+++ photos/models.py
@@ -3,3 +3,16 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     location=models.CharField(max_length=30)
+
+    def __str__(self):
+       return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.name
+
+
+
