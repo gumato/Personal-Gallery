6aee87e4b9684efd88139714e7daaad039c16a59
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-10 17:21:19 +0300

    Add image models

--- photos/models.py
+++ photos/models.py
@@ -14,5 +14,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Image(models.Model):
+    image = models.CharField(max_length =60)
+    image_name = models.CharField(max_length =20)
+    image_description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+
 
 
