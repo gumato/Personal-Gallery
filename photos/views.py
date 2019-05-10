710918347f9fa71fbb0989f902ca199c6f80dbe3
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-10 16:12:09 +0300

    Create models

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,9 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 from django.shortcuts import render
 import datetime as dt
 
 # Create your views here.
 def mygallery(request):
     date = dt.date.today()
-    # photos = Image.get_all()
-
+     
     return render(request,'welcome.html',{"date": date})
