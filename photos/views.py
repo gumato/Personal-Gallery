0340f77d9649b3d3bba7f728fb70df185adef7de
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-10 11:38:01 +0300

    Create templates folder

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
+def mygallery(request):
+    date = dt.date.today()
+    photos = Image.get_all()
+
+    return render(request,'all-photos/home.html',{"date": date,"photos":photos})
