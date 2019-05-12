ae3d1c7bd10db3893b61e3d8e82a2c8f4cd863dd
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-12 13:31:11 +0300

    Adding search template

--- photos/models.py
+++ photos/models.py
@@ -5,7 +5,13 @@ class Location(models.Model):
     location=models.CharField(max_length=30)
 
     def __str__(self):
-       return self.name
+       return self.location
+    
+    def save_locations(self):
+        self.save()
+
+    def delete_locations(self):
+        self.delete()
 
 
 class Category(models.Model):
@@ -13,14 +19,48 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 class Image(models.Model):
-    image = models.CharField(max_length =60)
-    image_name = models.CharField(max_length =20)
-    image_description = models.TextField()
+    image = models.ImageField(upload_to='photo/')
+    image_name = models.CharField(max_length =20,blank=False)
+    image_description = models.TextField(max_length=80,blank=False)
     location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    category = models.ManyToManyField(Category)
+    location = models.ForeignKey(Location)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+       return self.image_name
+    class Meta:
+        ordering = ["-comments_posts__timestamp","-comments_post__updated"]
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+    
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.all()
+        return images
+    
+    @classmethod
+    def filter_category(cls,query):
+        images = cls.objects.filter(category__name=query)
+        return images
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        images = cls.objects.filter(category__name__icontains = search_term).order_by('-pub_date_posted')
+        return images   
 
 
 
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
 # Create your tests here.
+class Location TestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.priclla= Location(location = 'America')
+ # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pricilla,Location))       
+
+        
